9df8ee94eb1fe202ead2e017ca1a305b7323f025
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-09-13 19:31:05 +0530

    Secure our API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -16,12 +17,22 @@ app.use(cookieParser());
 
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
 
-// Routes
-app.use('/auth', authRoutes);
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
-  logger.info('Hello from Acquisitions!');
-  res.status(200).send('Hello from Acquisitions!');
+logger.info('Hello from Acquisitions!');
+
+res.status(200).send('Hello from Acquisitions!');
 });
+
+app.get('/health', (req, res) => {
+res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api', (req, res) => {
+res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
