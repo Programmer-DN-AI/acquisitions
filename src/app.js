1a6f8e6bd65ba1889be735314be070c2c99e136b
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-09-14 17:19:37 +0530

    Implement tests

--- src/app.js
+++ src/app.js
@@ -37,4 +37,8 @@ res.status(200).json({ message: 'Acquisitions API is running!' });
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+})
+
 export default app;
